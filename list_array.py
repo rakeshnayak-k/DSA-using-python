30030730fc95cefa4f2c6f45123e8a616bf35892
rakeshnayal-k <rakeshdravid25@gmail.com>
2023-11-17 12:17:34 +0530

    list and array in python

---
+++ list_array.py
@@ -0,0 +1,21 @@
+import array as array
+
+# array in python
+a1 = array.array('i',[1,2,3])
+
+print(type(a1))
+print(a1)
+
+a1.append(4)
+a1.insert(0,2)
+
+print(a1)
+for i in a1:
+    print(i)
+
+
+# list
+
+l = [1,3,'e','',9.0,None]
+for i in l:
+    print(i)
