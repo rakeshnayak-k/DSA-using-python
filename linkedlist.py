6bd6060c453769c2a80cd762f943830811b7b924
rakeshnayal-k <rakeshdravid25@gmail.com>
2023-11-27 08:00:12 +0530

    complete single linked list

--- linkedlist.py
+++ linkedlist.py
@@ -73,7 +73,60 @@ class SingleLinkedList:
         temp = self.head      
         for _ in range(index):
             temp = temp.next
-        return temp.value
+        return temp
+    
+    def set_value(self, index, value):
+        temp = self.get(index)
+        if temp:
+            # print('index value ' , temp)
+            temp.value = value
+
+            return True
+        return False
+    
+    def insert_value(self, index, value):
+        if index < 0 or index >= self.length:
+            return None
+        if index == 0:
+            return self.prepend(value)
+        if index == self.length:
+            return self.append(value)
+        new_node = Node(value)
+        temp = self.get(index -1)
+        new_node.next = temp.next
+        temp.next = new_node
+        self.length +=1
+        return True
+    
+    def remove(self, index):
+        if index < 0 or index >= self.length:
+            return None
+        if index == 0:
+            return self.pop_first()
+        if index == self.length-1:
+            return self.pop()
+        prev = self.get(index - 1)
+        temp = prev.next
+        prev.next = temp.next
+        temp.next = None
+        self.length -= 1
+        return temp
+    
+    def reverse(self):
+        temp = self.head
+        self.head = self.tail
+        self.tail = temp
+        after = temp.next
+        before = None
+        for _ in range(self.length):
+            after = temp.next
+            temp.next = before
+            before = temp
+            temp = after
+            
+        
+        
+
 
 
 
@@ -88,9 +141,17 @@ ll.append(1)
 ll.append(2)
 ll.append(3)
 
-ll.print_llist()
-print(ll.get(4))
+# ll.set_value(1,10)
+# ll.insert_value(3,10)
 
+# ll.remove(2)
+ll.reverse()
+
+ll.print_llist()
+# print(ll.get(0).value)
+
+
+# ll.print_llist()
 
 # print('pop ',ll.pop_first())
 # print('pop ',ll.pop())
