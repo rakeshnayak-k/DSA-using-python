8c7485de70977ab97474ba237e0e4b66333f9de7
rakeshnayal-k <rakeshdravid25@gmail.com>
2023-11-28 23:41:23 +0530

    Merge Sort

---
+++ merge_sort.py
@@ -0,0 +1,39 @@
+def merge(list1, list2):
+    combined = []
+    i = 0
+    j = 0
+    while i < len(list1) and j < len(list2):
+        if list1[i] < list2[j]:
+            combined.append(list1[i])
+            i += 1
+        else:
+            combined.append(list2[j])
+            j += 1
+    while i < len(list1):
+        combined.append(list1[i])
+        i += 1
+    while j < len(list2):
+        combined.append(list2[j])
+        j += 1
+    return combined
+
+"""
+1. break lists in half
+2. base case: when len(list) is 1
+3. uses merge to put list togther """
+
+def merge_sort(my_list):
+    if len(my_list) == 1:
+        return my_list
+    mid_index = int(len(my_list)/2)
+    left = merge_sort(my_list[:mid_index])
+    right = merge_sort(my_list[mid_index:])
+
+    return merge(left, right)
+
+
+l = [3,1,4,2,-1]
+
+l2 = merge_sort(l)
+print(l2)
+                
