7a0975d03c0783b7b23884e0df33d52432f2afad
rakeshnayal-k <rakeshdravid25@gmail.com>
2023-12-01 21:22:32 +0530

    Tree Traversal

--- trees.py
+++ trees.py
@@ -41,9 +41,59 @@ class BinarySearchTree:
                 return True
         return False
 
-
-        
-
+    # Tree Traversal (BFS - Breadth First Search)
+    def bfs(self):
+        current_node = self.root
+        queue = []
+        results = []
+        queue.append(current_node)
+        while len(queue) > 0:
+            current_node = queue.pop(0)
+            results.append(current_node.value)
+            if current_node.left is not None:
+                queue.append(current_node.left)
+            if current_node.right is not None:
+                queue.append(current_node.right)
+        return results
+    
+    # Tree Traversal 
+    # (DFS - Depth First Search 3 types: PreOrder, PostOrder, InOrder)
+    # PreOrder
+    def dfs_pre_order(self):
+        results = []
+        def traverse(current_node):
+            results.append(current_node.value)
+            if current_node.left is not None:
+                traverse(current_node.left)
+            if current_node.right is not None:
+                traverse(current_node.right)
+        traverse(self.root)
+        return results
+    
+    # PostOrder
+    def dfs_post_order(self):
+        results = []
+        def traverse(current_node):
+            if current_node.left is not None:
+                traverse(current_node.left)
+            if current_node.right is not None:
+                traverse(current_node.right)
+            results.append(current_node.value)
+        traverse(self.root)
+        return results
+    
+    # InOrder
+    def dfs_in_order(self):
+        results = []
+        def traverse(current_node):
+            if current_node.left is not None:
+                traverse(current_node.left)
+            results.append(current_node.value)
+            if current_node.right is not None:
+                traverse(current_node.right)
+        traverse(self.root)
+        return results
+    
 
 bst = BinarySearchTree()
 bst.insert(47)
@@ -55,9 +105,18 @@ bst.insert(52)
 bst.insert(82)
 
 
-print(bst.contains(47))
-print(bst.contains(27))
-print(bst.contains(17))
+# print(bst.bfs())
+
+print(bst.dfs_pre_order())
+print(bst.dfs_post_order())
+print(bst.dfs_in_order())
+
+
+
+
+# print(bst.contains(47))
+# print(bst.contains(27))
+# print(bst.contains(17))
 
 
 # print(bst.root.value)
